f41b3b22a518a60fc6fef61315a996606700abfb
bryan <bryanlockkamper2@gmail.com>
2019-12-09 13:59:25 +0100

    reparation

---
+++ AgeOfSushiV1/src/com/company/models/Batiment.java
@@ -0,0 +1,44 @@
+package com.company.models;
+
+import java.util.List;
+
+public class Batiment  extends Entity{
+    List<Entity> entities;
+    Entity prod;
+    List<Ressource> requirement;
+
+    public Batiment(int pv, String name, List<Entity> entities, Entity prod, List<Ressource> requirement) {
+        super(pv, name);
+        this.entities = entities;
+        this.prod = prod;
+        this.requirement = requirement;
+    }
+
+    public List<Entity> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(List<Entity> entities) {
+        this.entities = entities;
+    }
+
+    public Entity getProd() {
+        return prod;
+    }
+
+    public void setProd(Entity prod) {
+        this.prod = prod;
+    }
+
+    public List<Ressource> getRequirement() {
+        return requirement;
+    }
+
+    public void setRequirement(List<Ressource> requirement) {
+        this.requirement = requirement;
+    }
+
+    public Entity produire(Entity entity) {
+        return entity;
+    }
+}
