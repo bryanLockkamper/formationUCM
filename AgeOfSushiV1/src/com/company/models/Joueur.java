3bfd39c6e24b21b1fe23e005e1cc9cb9579ac786
bryan <bryanlockkamper2@gmail.com>
2019-12-09 13:42:54 +0100

    pre-merge

--- AgeOfSushiV1/src/com/company/models/Joueur.java
+++
@@ -1,9 +0,0 @@
-package com.company.models;
-
-import java.util.List;
-
-public class Joueur {
-    protected String name;
-    protected List<Ressource> ressources;
-    protected List<Entity> entities;
-}
---
+++ test/src/com/company/models/Joueur.java
@@ -0,0 +1,9 @@
+package com.company.models;
+
+import java.util.List;
+
+public class Joueur {
+    protected String name;
+    protected List<Ressource> ressources;
+    protected List<Entity> entities;
+}
