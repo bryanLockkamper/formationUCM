3b15ac72231e7acdfcde9f2a60650c4c94419da1
bryan <bryanlockkamper2@gmail.com>
2019-12-10 16:26:40 +0100

    jour 1

--- UcmEmpire/src/main/java/controllers/Jeu.java
+++ UcmEmpire/src/main/java/controllers/Jeu.java
@@ -1,7 +1,23 @@
 package controllers;
 
+import models.Joueur;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Jeu {
     public Jeu() {
+        Joueur joueur = new Joueur();
+        Joueur ia = new Joueur();
+        boolean victoire = false;
+        boolean perdu = false;
+        while (!victoire | !perdu) {
+            //avant tour (ressource)
+            //tour
 
+            //après tour (deplacement auto)
+            perdu = joueur.aPerdu();
+            victoire = ia.aPerdu();
+        }
     }
 }
--- UcmEmpire/src/main/java/models/Joueur.java
+++ UcmEmpire/src/main/java/models/Joueur.java
@@ -6,4 +6,32 @@ public class Joueur {
     protected String name;
     protected List<Ressource> ressources;
     protected List<Entity> entities;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ressource> getRessources() {
+        return ressources;
+    }
+
+    public void setRessources(List<Ressource> ressources) {
+        this.ressources = ressources;
+    }
+
+    public List<Entity> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(List<Entity> entities) {
+        this.entities = entities;
+    }
+
+    public boolean aPerdu() {
+        return true;
+    }
 }
