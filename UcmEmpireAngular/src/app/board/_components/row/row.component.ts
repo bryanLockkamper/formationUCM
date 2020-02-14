1cf13306187335487fc1483eb7937ac0fca5230b
bryan <bryanlockkamper2@gmail.com>
2020-02-14 15:51:21 +0100

    Refresh board to move

---
+++ UcmEmpireAngular/src/app/board/_components/row/row.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'row',
+  templateUrl: './row.component.html',
+    styleUrls: [ '../board/board.component.scss' ]
+})
+
+export class RowComponent implements OnInit {
+  @Input() dimension: number;
+  @Input() id: number;
+  cells = [];
+  style: string;
+  @Output() cell = new EventEmitter<{rowId: number, id: number}>();
+
+  ngOnInit() {
+    this.style = 'square pure-u-1-' + this.dimension;
+    for (let i = 0; i < this.dimension; i++) {
+      this.cells.push({
+        rowId: this.id,
+        id: i,
+      });
+    }
+  }
+
+  onCellClick(cell) {
+    this.cell.emit(cell);
+  }
+}
