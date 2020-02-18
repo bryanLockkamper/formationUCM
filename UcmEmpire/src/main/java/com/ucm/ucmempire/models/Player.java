f47282cb6bacd1dcea68668ffb3b1e850df83a7b
bryan <bryanlockkamper2@gmail.com>
2020-02-18 16:31:28 +0100

    Merge branch 'feature/combatAngular' into devSpring
    # Conflicts:
    #	UcmEmpire/src/main/java/com/ucm/ucmempire/controllers/Game.java
    #	UcmEmpire/src/main/java/com/ucm/ucmempire/models/Character.java
    #	UcmEmpire/src/main/java/com/ucm/ucmempire/models/Player.java
    #	UcmEmpire/src/main/java/com/ucm/ucmempire/models/units/Farmer.java
    #	UcmEmpire/src/main/java/com/ucm/ucmempire/models/units/Soldier.java

--- UcmEmpire/src/main/java/com/ucm/ucmempire/models/Player.java
+++ UcmEmpire/src/main/java/com/ucm/ucmempire/models/Player.java
@@ -1,5 +1,6 @@
 package com.ucm.ucmempire.models;
 
+import com.ucm.ucmempire.models.buildings.Building;
 import com.ucm.ucmempire.models.buildings.Granary;
 import com.ucm.ucmempire.models.buildings.buildingInterfaces.IProdBuilding;
 import com.ucm.ucmempire.models.resources.Resource;
