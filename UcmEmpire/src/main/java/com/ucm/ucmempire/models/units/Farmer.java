fed1e6d51d6174cb69578e5cff417384850a45c8
Comamanel <florent.piette9@gmail.com>
2020-02-20 14:51:06 +0100

    added farmer inventory, when harvesting, the farmer gets the resource

--- UcmEmpire/src/main/java/com/ucm/ucmempire/models/units/Farmer.java
+++ UcmEmpire/src/main/java/com/ucm/ucmempire/models/units/Farmer.java
@@ -2,12 +2,23 @@ package com.ucm.ucmempire.models.units;
 
 import com.ucm.ucmempire.models.Character;
 import com.ucm.ucmempire.models.Constants;
+import com.ucm.ucmempire.models.resources.Resource;
 import com.ucm.ucmempire.models.resources.ResourceName;
 import com.ucm.ucmempire.models.units.unitInterfaces.IFarmer;
+import lombok.ToString;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@ToString
 public class Farmer extends Character implements IFarmer {
 
     private ResourceName resourceHarvesting;
+
+    private Map<ResourceName, Integer> inventory;
+
 
     public Farmer(int idUser) {
         super(idUser, Constants.NB_POINTDEVIE, Constants.NB_POINTACTION);
@@ -17,8 +28,8 @@ public class Farmer extends Character implements IFarmer {
 
     public Farmer(Integer idUser, int hp, int pa) {
         super(idUser, hp, pa);
+        this.setInventory();
     }
-
 
     public ResourceName getResourceHarvesting(){
         return resourceHarvesting;
@@ -26,5 +37,17 @@ public class Farmer extends Character implements IFarmer {
 
     public void setResourceHarvesting(ResourceName resourceHarvesting){
         this.resourceHarvesting = resourceHarvesting;
+    }
+
+    public Map<ResourceName, Integer> getInventory() {
+        return this.inventory;
+    }
+
+
+    private void setInventory(){
+        this.inventory = new HashMap<>();
+        this.inventory.put(ResourceName.STONE, 0);
+        this.inventory.put(ResourceName.FOOD, 0);
+        this.inventory.put(ResourceName.WOOD, 0);
     }
 }
