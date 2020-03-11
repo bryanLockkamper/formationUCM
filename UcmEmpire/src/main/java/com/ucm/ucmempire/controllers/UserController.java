6262127f63a0585d92aedb2902d7cb839349e5b9
Damien <damien.fricot@hotmail.be>
2020-03-11 15:10:48 +0100

    security and token creation

---
+++ UcmEmpire/src/main/java/com/ucm/ucmempire/controllers/UserController.java
@@ -0,0 +1,79 @@
+package com.ucm.ucmempire.controllers;
+
+import com.ucm.ucmempire.configuration.HashConfig;
+import com.ucm.ucmempire.configuration.JwtTokenProvider;
+import com.ucm.ucmempire.dal.entity.PlayerEntity;
+import com.ucm.ucmempire.dal.entity.Role;
+import com.ucm.ucmempire.dal.servicedal.PlayerDalService;
+import com.ucm.ucmempire.dal.servicedal.PlayerDalServiceImpl;
+import com.ucm.ucmempire.models.Player;
+import com.ucm.ucmempire.models.dto.PlayerDTOLogin;
+import com.ucm.ucmempire.models.dto.PlayerDTORegister;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    HashConfig hashConfig;
+
+    @Autowired
+    PlayerDalService playerDalService;
+
+    @ApiOperation(value = "Appelé a chaque fois qu'un joueur veut se connecter")
+    @PostMapping("/login")
+    public ResponseEntity signin(@RequestBody PlayerDTOLogin data)
+    {
+        try {
+            String login = data.getPseudo();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(login,(data.getPassword())));
+            String token = jwtTokenProvider.createToken(login,this.playerDalService.findByLogin(login).orElseThrow(() -> new UsernameNotFoundException("Login "+ login+ "not found")).getRoles().stream().map(Role::getAuthority).collect(Collectors.toList()));
+            Map<Object,Object> model = new HashMap<>();
+            model.put("login",login);
+            model.put("token",token);
+            System.out.println("coucou user :"+login);
+            return ResponseEntity.ok(model);
+        } catch (AuthenticationException e)
+        {
+            throw new BadCredentialsException("Invalid login/password");
+        }
+    }
+
+    @ApiOperation(value = "Appelé a chaque fois qu'un joueur voudrat s'enregistrer")
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody PlayerDTORegister playerDTO) {
+
+        playerDTO.setPassword(hashConfig.getPasswordEncoder().encode(playerDTO.getPassword()));
+        PlayerEntity playerEntity = new PlayerEntity(playerDTO);
+        playerDalService.save(playerEntity);
+
+
+        return ResponseEntity.ok("200");
+    }
+
+
+}
