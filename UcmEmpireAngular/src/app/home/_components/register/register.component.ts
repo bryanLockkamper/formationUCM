0785aeab5b5ab5c85a9fb527caa42f48d803a507
Damien <damien.fricot@hotmail.be>
2020-03-12 13:42:06 +0100

    register toastr

--- UcmEmpireAngular/src/app/home/_components/register/register.component.ts
+++ UcmEmpireAngular/src/app/home/_components/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { UserRegister } from '../../_models/user-register';
 import { SecurityService } from '../../security.service';
 import { Router } from '@angular/router';
+import {NbToastrService} from '@nebular/theme';
 
 @Component({
   selector: 'app-register',
@@ -18,6 +19,8 @@ export class RegisterComponent implements OnInit {
   constructor(
     private userServ: SecurityService,
     private routServ: Router,
+    private toastrServ : NbToastrService,
+
   ) { }
 
   ngOnInit(): void {
@@ -34,7 +37,15 @@ export class RegisterComponent implements OnInit {
   {
     this.usermodel = this.userform.value;
 
-    this.userServ.register(this.usermodel).subscribe();
+    this.userServ.register(this.usermodel).subscribe(
+      (model) => {
+        this.toastrServ.success('Bienvenu dans notre communauté ' + this.usermodel.pseudo,'Enregistrement', {[status]:'success'});
+      },
+      () => {
+        this.toastrServ.success('Erreur dans l"enregistrement ','Enregistrement', {[status]:'danger'});
+      },
+      () => {},
+    );
     this.routServ.navigateByUrl('/board')
   }
 
